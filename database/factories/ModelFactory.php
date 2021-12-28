38aac0b155f37e3f26301ce29b5bba2f72732921
Alexandru Eftimie - mac <eftimie.alexandru@gmail.com>
2021-12-28 17:24:59 +0200

    Initial commit

--- database/factories/ModelFactory.php
+++ database/factories/ModelFactory.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace VendorName\Skeleton\Database\Factories;
+namespace Ideatocode\BladeStacksPusher\Database\Factories;
 
 use Illuminate\Database\Eloquent\Factories\Factory;
 
--- tests/Pest.php
+++ tests/Pest.php
@@ -1,5 +1,5 @@
 <?php
 
-use VendorName\Skeleton\Tests\TestCase;
+use Ideatocode\BladeStacksPusher\Tests\TestCase;
 
 uses(TestCase::class)->in(__DIR__);
