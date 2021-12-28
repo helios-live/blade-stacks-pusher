b12ff1fa0393a6cb05ba5ffda44ea6feab3ede35
Alexandru Eftimie - mac <eftimie.alexandru@gmail.com>
2021-12-28 17:36:14 +0200

    fixed the facade

--- src/BladeStacksPusherServiceProvider.php
+++ src/BladeStacksPusherServiceProvider.php
@@ -2,8 +2,15 @@
 
 namespace Ideatocode\BladeStacksPusher;
 
+use Illuminate\Support\Facades\App;
 use Illuminate\Support\ServiceProvider;
 
 class BladeStacksPusherServiceProvider extends ServiceProvider
 {
+    public function register()
+    {
+        App::bind('blade-stacks-pusher', function () {
+            return new BladeStacksPusher;
+        });
+    }
 }
